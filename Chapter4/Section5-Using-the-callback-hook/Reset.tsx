745256dfdb7bef9a68161f8330dd914ba71b3ca5
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-07-06 18:09:08 +0100

    Ref, memo and callback hooks

---
+++ Chapter4/Section5-Using-the-callback-hook/Reset.tsx
@@ -0,0 +1,8 @@
+import { memo } from "react";
+type Props = {
+  onClick: () => void;
+};
+export const Reset = memo(({ onClick }: Props) => {
+  console.log("render Reset");
+  return <button onClick={onClick}>Reset</button>;
+});
