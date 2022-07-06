32121850cc09d35e9ee4724df47ec6f5ce116c50
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-07-06 05:19:03 +0100

    Chapter4 - effect and state hooks

---
+++ Chapter4/Section1-Using-the-effect-hook/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { PersonScore } from "./PersonScore";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PersonScore />
+    </div>
+  );
+}
